7d6d1f7318897ad93de7eed73af62f8492cde7d4
Vincent Mendiola <vincent.mendiola.ii@gmail.com>
2025-03-16 23:24:14 -0400

    Add failing spec for required arguments for WeatherService

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe WeatherService, type: :service do
+  let(:lat) { 37.3182932 }
+  let(:lon) { -121.9509885981505 }
+
+  it "should require the lat and lon arguments" do
+    expect {
+      described_class.new
+    }.to raise_error(ArgumentError, "The lat and lon arguments are required.")
+  end
 end
