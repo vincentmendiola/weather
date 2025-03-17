a36f51d5189c36bd6d6884643ce77ae1f1386bda
Vincent Mendiola <vincent.mendiola.ii@gmail.com>
2025-03-16 23:34:07 -0400

    Add failing specs for getting the weather_info for the forecast

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe WeatherService, type: :service do
       described_class.new
     }.to raise_error(ArgumentError, "The lat and lon arguments are required.")
   end
+
+  it "should raise an exception when the weather_info returns an empty Hash" do
+    allow_any_instance_of(described_class).to receive(:weather_info).and_return({})
+    weather_service = described_class.new(lat: lat, lon: lon)
+    expect {
+      weather_service.current_temperature
+    }.to raise_error(StandardError, "Unable to process the request to api.weather.gov.")
+  end
+
+  it "should raise an exception when the api.weather.gov forecast endpoint returns an empty Hash" do
+    allow_any_instance_of(described_class).to receive(:weather_info).and_return({ "properties" => { "forecast" => {} } })
+    allow_any_instance_of(JSON).to receive(:parse).and_return({})
+    weather_service = described_class.new(lat: lat, lon: lon)
+    expect {
+      weather_service.current_temperature
+    }.to raise_error(StandardError, "Unable to process the request to api.weather.gov.")
+  end
 end
