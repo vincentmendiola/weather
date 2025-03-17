64cfb45312742cdd0eb528ecf0fe2dd6cb2af3c2
Vincent Mendiola <vincent.mendiola.ii@gmail.com>
2025-03-17 00:02:58 -0400

    Add logic to green spec for missing address using OpenStreetMap

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,9 +4,60 @@ class WeatherController < ApplicationController
   def index
   end
 
+  # Uses the OpenStreetMap and WeatherService to retrieve the weather forecast.
+  # 
+  # == OpenStreetMap Response Object
+  # {
+  #   "place_id"=>297206296,
+  #   "licence"=>
+  #   "Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright",
+  #   "osm_type"=>"relation",
+  #   "osm_id"=>13734358,
+  #   "lat"=>"37.3182932",
+  #   "lon"=>"-121.9509885981505",
+  #   "class"=>"historic",
+  #   "type"=>"heritage",
+  #   "place_rank"=>30,
+  #   "importance"=>0.405994309706842,
+  #   "addresstype"=>"historic",
+  #   "name"=>"Winchester Mystery House",
+  #   "display_name"=>
+  #   "Winchester Mystery House, 525, Winchester Boulevard, San Jose, Santa Clara County, California, 95128, United States",
+  #   "address"=>{
+  #     "historic"=>"Winchester Mystery House",
+  #     "house_number"=>"525",
+  #     "road"=>"Winchester Boulevard",
+  #     "city"=>"San Jose",
+  #     "county"=>"Santa Clara County",
+  #     "state"=>"California",
+  #     "ISO3166-2-lvl4"=>"US-CA",
+  #     "postcode"=>"95128",
+  #     "country"=>"United States",
+  #     "country_code"=>"us"
+  #   },
+  #   "boundingbox"=>[
+  #     "37.3178410",
+  #     "37.3187484",
+  #     "-121.9517206",
+  #     "-121.9506855"
+  #   ]
+  # }
   def forecast
     begin
-      weather_params
+      client = OpenStreetMap::Client.new
+      address_result = client.search(q: weather_params.join(" "),
+                                      format: 'json',
+                                      addressdetails: '1',
+                                      accept_language: 'en'
+                                    ).try(:first)
+
+      if address_result.present?
+        # TODO: Add WeatherService
+      else
+        flash[:alert] = "The provided address could not be found. Please try again."
+
+        render status: 404
+      end
     rescue ActionController::ParameterMissing => e
       first_letter = e.message.first.upcase
       message_remainder = e.message[1..e.message.size]
