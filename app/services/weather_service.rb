05807034e804255a0552fcf61a5ce950bcd15f17
Vincent Mendiola <vincent.mendiola.ii@gmail.com>
2025-03-16 23:26:53 -0400

    Add logic to green specs for required arguments in WeatherService

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,2 +1,19 @@
 class WeatherService
+  attr_reader :lat, :lon
+
+  def initialize(lat: nil, lon: nil)
+    @lat = formatted_point(lat)
+    @lon = formatted_point(lon)
+  end
+
+  private
+
+  def formatted_point(point)
+    # TODO: Include error handling for improperly formatted latitude and longitude.
+    formatted = point.try(:to_f).try(:round, 4).try(:to_s)
+
+    raise ArgumentError, "The lat and lon arguments are required." if formatted.nil?
+
+    formatted
+  end
 end
