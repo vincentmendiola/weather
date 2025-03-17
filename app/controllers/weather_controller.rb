a708b8432b092dc69e0743c22a8d495536bd15e7
Vincent Mendiola <vincent.mendiola.ii@gmail.com>
2025-03-16 23:18:16 -0400

    Green weather#forecast required params specs
    * Added handling of error messages using `flash[:alert]`.
    * Added `weather_params` to handle required params.
    * Added route, controller action, and view.

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,31 @@
 class WeatherController < ApplicationController
+  after_action -> { flash.discard }
+
   def index
+  end
+
+  def forecast
+    begin
+      weather_params
+    rescue ActionController::ParameterMissing => e
+      first_letter = e.message.first.upcase
+      message_remainder = e.message[1..e.message.size]
+      flash[:alert] = first_letter + message_remainder
+
+      render status: 422
+    end
+  end
+
+  private
+
+  def weather_params
+    params.require(
+      [
+        :street,
+        :city,
+        :state,
+        :zip_code
+      ]
+    )
   end
 end
